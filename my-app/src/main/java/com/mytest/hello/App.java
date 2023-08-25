a74c0b119ffe10b58deb383025feed082989689a
meet raj <mmeetengineer@gmail.com>
2023-08-25 13:35:14 +0530

    second commit

---
+++ my-app/src/main/java/com/mytest/hello/App.java
@@ -0,0 +1,13 @@
+package com.mytest.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This is JAVA Project!" );
+    }
+}
